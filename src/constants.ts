e86ddb5a09d532bbd941ffa93958180af5d95e88
Sachin-Kinha <sachinkinha78@gmail.com>
2023-08-04 17:50:03 +0530

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,28 @@
+import {Track} from 'react-native-track-player';
+
+export const playListData: Track[] = [
+  {
+    id: 1,
+    title: 'Baby doll',
+    artist: 'Ragini MMS 2',
+    album: 'Meet Bros Kanika Kapoor',
+    artwork: 'https://img.youtube.com/vi/5qAy_cbd3C8/hqdefault.jpg',
+    url: require('./assets/audio/baby.mp3'),
+  },
+  {
+    id: 2,
+    title: 'Ak 47 ',
+    artist: 'Diljit Dosanjh',
+    album: 'Punjabi',
+    artwork: 'https://i.ytimg.com/vi/x4-VHs39Efw/maxresdefault.jpg',
+    url: require('./assets/audio/Ak47.mp3'),
+  },
+  {
+    id: 3,
+    title: 'Kade has bol',
+    artist: 'Atif Aslam',
+    album: 'Bollywood',
+    artwork: 'https://i.ytimg.com/vi/nw2YmXNUpNU/hqdefault.jpg',
+    url: require('./assets/audio/Kadi_Has_Bol.mp3'),
+  },
+];
